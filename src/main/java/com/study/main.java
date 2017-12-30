37ce20b02b1dec9db082164d5c9ef4ca7f81eb27
AndriiDidkivsky <andrii.didkivsky@gmail.com>
2017-12-30 22:22:03 +0200

    merge sorting

--- src/main/java/com/study/main.java
+++ src/main/java/com/study/main.java
@@ -3,10 +3,16 @@ import com.study.sorting.*;
 
 class Main {
     public static void main(String[] args) {
-        InsertSort s1 = new InsertSort(20);
+        InsertSort s1 = new InsertSort(30);
         s1.fillRandom();
         s1.print();
         s1.sort();
         s1.print();
-    }
+
+        MergeSort s2 = new MergeSort(30);
+        s2.fillRandom();
+        s2.print();
+        s2.sort();
+        s2.print();
+    } 
 } 
---
+++ src/main/java/com/study/sorting/MergeSort.java
@@ -0,0 +1,53 @@
+package com.study.sorting;
+
+import java.nio.Buffer;
+
+public class MergeSort extends AbstractSort { 
+    
+    public MergeSort(int size) {
+        super(size);
+    } 
+
+
+    public void sort() {
+        int[] buffer = new int[size];
+        mergeSort(buffer, 0, size - 1);
+    } 
+
+    private void mergeSort(int buffer[], int left, int right) {
+        if(left == right) {
+            return;
+        }
+        
+        int middle = (left + right) / 2;
+
+        mergeSort(buffer, left, middle);
+
+        mergeSort(buffer, middle + 1, right);
+
+        merge(buffer, left, middle, right);
+    } 
+
+
+    private void merge(int buffer[], int left, int right, int top) {
+        int l = left;
+        int r = right + 1;
+        int mid = right;
+        int i = 0;
+        int length = top - left + 1;
+    
+        while (l <= mid && r <= top) {
+            buffer[i++] = arr[l] < arr[r] ? arr[l++] : arr[r++];
+        }
+        while (l <= mid) {
+            buffer[i++] = arr[l++];
+        } 
+        while (r <= top) {
+            buffer[i++] = arr[r++];
+        } 
+
+        for(i = 0; i < length; i++) {
+            arr[left + i] = buffer[i];
+        }
+    } 
+}
